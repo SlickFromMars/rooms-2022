47b12dcf73aa513097c366e8da718fd000b89e6b
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2022-09-15 09:15:23 -0400

    first commit

---
+++ source/Paths.hx
@@ -0,0 +1,36 @@
+package;
+
+class Paths
+{
+	inline public static var SOUND_EXT = #if web "mp3" #else "ogg" #end;
+
+	inline public static function getPath(file:String)
+	{
+		return 'assets/$file';
+	}
+
+	inline static public function txt(key:String)
+	{
+		return getPath('data/$key.txt');
+	}
+
+	inline static public function json(key:String)
+	{
+		return getPath('data/$key.json');
+	}
+
+	inline static public function sound(key:String)
+	{
+		return getPath('sounds/$key$SOUND_EXT');
+	}
+
+	inline static public function music(key:String)
+	{
+		return getPath('music/$key$SOUND_EXT');
+	}
+
+	inline static public function image(key:String)
+	{
+		return getPath('images/$key.png');
+	}
+}
---
+++ source/TitleState.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxState;
+
+class TitleState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
