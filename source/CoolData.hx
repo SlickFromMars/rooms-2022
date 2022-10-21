8a0249eb796185dfc4ff8c3bfdb93a9e400ee968
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2022-10-21 09:13:57 -0400

    collision fix

--- source/CoolData.hx
+++ source/CoolData.hx
@@ -4,5 +4,5 @@ class CoolData
 {
 	// TILE STUFF
 	public static var tileCount:Int = 12;
-	public static var doTileCollision:Array<Int> = [5, 6, 7, 8, 9, 10, 11, 12];
+	public static var doTileCollision:Array<Int> = [4, 5, 6, 7, 8, 9, 10, 11, 12];
 }
