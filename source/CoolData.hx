a35c4b5be4da6f22f133cba2c4d05893150b5e85
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2022-10-21 12:26:58 -0400

    prison update

--- source/CoolData.hx
+++ source/CoolData.hx
@@ -3,6 +3,6 @@ package;
 class CoolData
 {
 	// TILE STUFF
-	public static var tileCount:Int = 14;
-	public static var doTileCollision:Array<Int> = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+	public static var tileCount:Int = 15;
+	public static var doTileCollision:Array<Int> = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 }
