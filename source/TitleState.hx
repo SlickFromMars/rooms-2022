3a604433819106ec94bb9d875294e3bd388aa16d
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2023-01-12 09:28:48 -0500

    moving this back smh

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -3,12 +3,12 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.effects.FlxFlicker;
-import flixel.effects.particles.FlxEmitter;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import flixel.util.FlxGradient;
 import lime.app.Application;
+import particles.TitleEmitter;
 
 class TitleState extends FrameState
 {
@@ -136,22 +136,5 @@ class TitleState extends FrameState
 				});
 			});
 		}
-	}
-}
-
-class TitleEmitter extends FlxEmitter
-{
-	public function new(texture:String)
-	{
-		super(0, FlxG.height);
-
-		launchMode = SQUARE;
-		velocity.set(-25, -75, 25, -100, -50, 0, 50, -50);
-		scale.set(0.25, 0.25, 0.5, 0.5, 0.25, 0.25, 0.37, 0.37);
-		drag.set(0, 0, 0, 0, 5, 5, 10, 10);
-		width = FlxG.width;
-		alpha.set(0.7, 0.7, 0, 0);
-		lifespan.set(1, 3);
-		loadParticles(Paths.image('particles/$texture'), 500, 16, true);
 	}
 }
