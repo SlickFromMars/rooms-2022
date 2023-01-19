2ad82b577abd84c837333efa2666db1849093a02
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2023-01-19 11:19:49 -0500

    mb

--- source/particles/JumpEmitter.hx
+++ source/particles/JumpEmitter.hx
@@ -9,7 +9,6 @@ class JumpEmitter extends FlxEmitter
 		super(x, y);
 
 		launchMode = CIRCLE;
-		// velocity.set(-25, -75, 25, -100, -50, 0, 50, -50);
 		scale.set(0.1);
 		alpha.set(0.7, 0.7, 0, 0);
 		lifespan.set(0.5, 1);
