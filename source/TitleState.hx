6b5996f47de050cb392f6cea3adbbfb159f5ba42
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2023-01-26 15:22:57 -0500

    nvm take off the end

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -29,7 +29,7 @@ class TitleState extends FrameState
 	var emitterGrp:FlxTypedGroup<FlxEmitter>; // Particle group yaaaay
 
 	#if EASTER_EGG
-	var easterEggKeys:Array<String> = ['SLICKFROMMARS', 'ALPHAMEEP', 'DANNY', 'LOGIC', 'POKIADOKI', 'JADIN', 'WISEGIRL'];
+	var easterEggKeys:Array<String> = ['SLICK', 'ALPHAMEEP', 'DANNY', 'LOGIC', 'POKIADOKI', 'JADIN', 'WISEGIRL'];
 	var allowedKeys:String = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	var easterEggKeysBuffer:String = '';
 	#end
