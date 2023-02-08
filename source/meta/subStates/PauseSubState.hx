1f87b22031f40d412d8903ff8eb82045ecb2c841
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2023-02-08 10:47:46 -0500

    Update PauseSubState.hx

--- source/meta/subStates/PauseSubState.hx
+++ source/meta/subStates/PauseSubState.hx
@@ -15,14 +15,12 @@ class PauseSubState extends FrameSubState
 
 	var menuItems:Array<String> = [];
 	var menuItemsOG:Array<String> = ['Resume', 'Options', 'Exit To Menu'];
-	var optionItemsOG:Array<String> = ['Toggle Fullscreen'];
+	var optionItemsOG:Array<String> = ['Toggle Fullscreen', 'Toggle FPS Counter'];
 	var curSelected:Int = 0;
 
 	public function new()
 	{
 		super();
-
-		optionItemsOG.insert(1, 'Toggle FPS Counter');
 
 		#if debug
 		menuItemsOG.insert(1, 'Skip Level');
