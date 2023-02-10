bda71513912e2a2691fc99604048c82b9e0d72d8
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2023-02-10 12:37:07 -0500

    swaggy states

--- source/meta/states/CompleteState.hx
+++ source/meta/states/CompleteState.hx
@@ -1,9 +1,14 @@
 package meta.states;
 
 import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.effects.particles.FlxEmitter;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
+import flixel.util.FlxGradient;
 import meta.Frame.FrameState;
 #if discord_rpc
 import meta.Discord.DiscordClient;
@@ -12,7 +17,10 @@ import meta.Discord.DiscordClient;
 class CompleteState extends FrameState
 {
 	// The UI varaibles
-	var winText:FlxText;
+	var winText:FlxText; // the silly text
+	var screen:FlxSprite; // Funky gradient
+
+	var emitterGrp:FlxTypedGroup<FlxEmitter>; // Particle group yaaaay
 
 	override function create()
 	{
@@ -27,11 +35,35 @@ class CompleteState extends FrameState
 		#end
 
 		// Setup the UI
+		emitterGrp = new FlxTypedGroup<FlxEmitter>();
+
+		// Based off code from VSRetro, thanks guys
+		for (i in 0...2)
+		{
+			var emitter:FlxEmitter = new FlxEmitter(0, FlxG.height + 50);
+			emitter.launchMode = SQUARE;
+			emitter.velocity.set(-25, -75, 25, -100, -50, 0, 50, -50);
+			emitter.scale.set(0.25, 0.25, 0.5, 0.5, 0.25, 0.25, 0.37, 0.37);
+			emitter.drag.set(0, 0, 0, 0, 5, 5, 10, 10);
+			emitter.width = FlxG.width;
+			emitter.alpha.set(0.7, 0.7, 0, 0);
+			emitter.lifespan.set(1.5, 3);
+			emitter.loadParticles(Paths.image('particles/title$i'), 700, 16, true);
+			emitter.start(false, FlxG.random.float(0.4, 0.5), 100000);
+			emitterGrp.add(emitter);
+		}
+		screen = FlxGradient.createGradientFlxSprite(FlxG.width, Std.int(FlxG.height * 0.2), [FlxColor.TRANSPARENT, FlxColor.WHITE]);
+		screen.y = FlxG.height - screen.height;
+		screen.alpha = 0.7;
+
 		winText = new FlxText(0, 0, FlxG.width, '', 8);
 		updateUIText();
 		winText.alignment = CENTER;
 		winText.screenCenter(Y);
 		winText.alpha = 0;
+
+		add(emitterGrp);
+		add(screen);
 		add(winText);
 
 		super.create();
@@ -57,8 +89,9 @@ class CompleteState extends FrameState
 	function pressStart()
 	{
 		// Fade to black and then go to PlayState again
-		FlxG.sound.music.fadeOut(0.1);
-		FlxG.camera.fade(FlxColor.BLACK, 0.1, false, function()
+		FlxTween.tween(winText, {y: FlxG.height}, 1, {ease: FlxEase.quadIn});
+		FlxG.sound.music.fadeOut(0.7);
+		FlxG.camera.fade(FlxColor.BLACK, 1, false, function()
 		{
 			RoomsData.roomNumber = 1;
 			FrameState.switchState(new TitleState());
--- source/meta/states/OutdatedState.hx
+++ source/meta/states/OutdatedState.hx
@@ -2,10 +2,13 @@ package meta.states;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.effects.particles.FlxEmitter;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
+import flixel.util.FlxGradient;
 import meta.Frame.FrameState;
 
 #if CHECK_FOR_UPDATES
@@ -15,26 +18,54 @@ class OutdatedState extends FrameState
 
 	public static var updateVersion:String = '';
 
+	var emitterGrp:FlxTypedGroup<FlxEmitter>;
+	var screen:FlxSprite;
 	var warnText:FlxText;
 	var sillySprite:FlxSprite;
+	var sillyTween:FlxTween;
 
 	override function create()
 	{
 		super.create();
 
+		// Setup the UI
+		emitterGrp = new FlxTypedGroup<FlxEmitter>();
+
+		// Based off code from VSRetro, thanks guys
+		for (i in 0...2)
+		{
+			var emitter:FlxEmitter = new FlxEmitter(0, FlxG.height + 50);
+			emitter.launchMode = SQUARE;
+			emitter.velocity.set(-25, -75, 25, -100, -50, 0, 50, -50);
+			emitter.scale.set(0.25, 0.25, 0.5, 0.5, 0.25, 0.25, 0.37, 0.37);
+			emitter.drag.set(0, 0, 0, 0, 5, 5, 10, 10);
+			emitter.width = FlxG.width;
+			emitter.alpha.set(0.7, 0.7, 0, 0);
+			emitter.lifespan.set(1.5, 3);
+			emitter.loadParticles(Paths.image('particles/title$i'), 700, 16, true);
+			emitter.start(false, FlxG.random.float(0.4, 0.5), 100000);
+			emitterGrp.add(emitter);
+		}
+		screen = FlxGradient.createGradientFlxSprite(FlxG.width, Std.int(FlxG.height * 0.2), [FlxColor.TRANSPARENT, FlxColor.WHITE]);
+		screen.y = FlxG.height - screen.height;
+		screen.alpha = 0.7;
+
 		warnText = new FlxText(0, 0, FlxG.width, '', 14);
 		warnText.alignment = CENTER;
 		updateUIText();
-		add(warnText);
 
 		sillySprite = new FlxSprite(0, warnText.y + warnText.height - 30);
 		sillySprite.loadGraphic(Paths.image('slickfrommars'));
 		sillySprite.setGraphicSize(50);
 		sillySprite.screenCenter(X);
 		sillySprite.angle = -20;
+
+		add(emitterGrp);
+		add(screen);
+		add(warnText);
 		add(sillySprite);
 
-		FlxTween.tween(sillySprite, {angle: 20}, 3, {type: PINGPONG, ease: FlxEase.quadInOut});
+		sillyTween = FlxTween.tween(sillySprite, {angle: 20}, 3, {type: PINGPONG, ease: FlxEase.quadInOut});
 
 		FlxG.camera.fade(FlxColor.BLACK, 0.1, true);
 	}
@@ -55,8 +86,10 @@ class OutdatedState extends FrameState
 
 			if (leftState)
 			{
-				FlxTween.tween(warnText, {alpha: 0}, 1);
-				FlxG.camera.fade(FlxColor.BLACK, 1, false, function()
+				sillyTween.cancel();
+				FlxTween.tween(sillySprite, {y: FlxG.height, angle: 0}, 1, {ease: FlxEase.quadIn});
+				FlxTween.tween(warnText, {alpha: 0}, 1, {ease: FlxEase.quadIn});
+				FlxG.camera.fade(FlxColor.BLACK, 2, false, function()
 				{
 					FrameState.switchState(new meta.states.OpeningState());
 				});
